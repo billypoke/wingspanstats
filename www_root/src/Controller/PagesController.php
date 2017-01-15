c22e2f6ffec55de94d69cd8b5b7ebeded3b7d0fb
Kanly <andrei.negescu@mystage.ro>
2017-01-15 12:18:23 +0200

    added navigation

--- www_root/src/Controller/PagesController.php
+++ www_root/src/Controller/PagesController.php
@@ -18,7 +18,7 @@ use Cake\Core\Configure;
 use Cake\Network\Exception\ForbiddenException;
 use Cake\Network\Exception\NotFoundException;
 use Cake\View\Exception\MissingTemplateException;
-
+use Cake\Event\Event;
 /**
  * Static content controller
  *
@@ -28,7 +28,11 @@ use Cake\View\Exception\MissingTemplateException;
  */
 class PagesController extends AppController
 {
-
+    public function beforeFilter(Event $event){
+        parent::beforeFilter($event);
+    $options = array( 'agents', 'astero',  'blops', 'bombers',  'explorer',  'industry', 'interdictors', 'miner', 'nestor', 'recons',  'solo', 'stratios', 't3', 'team', 'thera');
+    $this->set('optionsMenu',$options);
+    }
     /**
      * Displays a view
      *
@@ -78,6 +82,63 @@ class PagesController extends AppController
         $this->set(compact('agentData','shipsData','stratiosData','bombersData','soloData','shipsChart','totalNave','generalData'));
     }
 
+    public function stats( $page = false,$date = false){
+        $this->viewBuilder()->layout('wingspan');
+        $prop = 'agents';
+        switch ($page) {
+            case 'agents': $file = 'agents';break;
+            case 'astero': $file = 'astero';break;
+            case 'awoxes': $file = 'awoxes'; $prop='kills'; break;
+            case 'blops': $file = 'blops';break;
+            case 'bombers': $file = 'bombers';break;
+            case 'bombing': $file = 'bombing_run_specialists';break;
+            case 'capitals': $file = 'capital_kills'; $prop='kills'; break;
+            case 'explorer': $file = 'explorer_hunter';break;
+            case 'stats': $file = 'general_stats';break;
+            case 'industry': $file = 'industry_giant';break;
+            case 'interdictors': $file = 'interdictor_ace';break;
+            case 'miner': $file = 'miner_bumper';break;
+            case 'nestor': $file = 'nestor';break;
+            case 'recons': $file = 'recons';break;
+            case 'ships': $file = 'ships';$prop='ships';break;
+            case 'solo': $file = 'solo_hunter';break;
+            case 'stratios': $file = 'stratios';break;
+            case 't3': $file = 't3cruiser';break;
+            case 'team': $file = 'team_player';break;
+            case 'thera': $file = 'thera_crusader';break;
+            default:               
+                $file = 'general_stats';
+                break;
+
+
+        }
+        if ($date == false){
+            $date = date('Y-m');
+        }else{
+            $d = explode('-',$date);
+            if (count($d) < 2) throw new ForbiddenException("Invalid date");
+            if ($d[1] < 1 && $d[0] < 2014) throw new ForbiddenException("Invalid date");
+        }
+          $root = 'results/'.$date.'/';
+        // $root = 'results/__alltime__/';
+        $data = file_get_contents($root.$file.'.json');
+        $parsedData = json_decode($data);
+        $parsedData = $parsedData->$prop;
+        if ($prop == 'agents'){
+            $propList = array(
+                'character_name',
+                    'ships_destroyed',
+                    'isk_destroyed',
+                    
+                );
+            $head = array(
+                'Agent',
+                'Ships destroied',
+                'Isk Destroyed',
+                );
+        }
+        $this->set(compact('parsedData','propList','head','page'));
+    }
     
     public function display()
     {
